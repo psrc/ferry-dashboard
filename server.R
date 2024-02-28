a1163a3ba8f5978ddd6ea8dc50e5eaff0644c45a
Craig Helmann <chelmann@psrc.org>
2024-02-27 17:06:00 -0800

    updated header layout

--- server.R
+++ server.R
@@ -5,11 +5,6 @@ shinyServer(function(input, output) {
   footer_server('psrcfooter')
   
   # Main Panel
-  banner_server('ferryBanner', 
-                banner_title = "Ferries", 
-                banner_subtitle = "Regional Transportation Plan",
-                banner_url = "https://www.psrc.org/planning-2050/regional-transportation-plan")
-  
   left_panel_server('leftMain', page_nm = "Main")
   ferry_overview_server('ferryOverview')
   ferry_ua_server('UAferry')
--- ui.R
+++ ui.R
@@ -12,10 +12,18 @@ shinyUI(
     title = "Ferry Use in the PSRC Region",
     
     theme = "styles.css",
-    banner_ui('ferryBanner'),
+    
+    fluidRow(column(4, tags$a(div(tags$img(src='psrc-logo.png',
+                                           style="margin-top: 10px; padding-left: 40px;",
+                                           height = "60")
+                                  ), href="https://www.psrc.org", target="_blank")),
+             column(8, br(), strong(tags$div(class="mainpage_title", "Ferry Usage in the Central Puget Sound")))),
+    
+    hr(style = "border-top: 1px solid #000000;"),
     
     sidebarLayout(
-      sidebarPanel(left_panel_ui('leftMain')),
+      sidebarPanel(#id="sidebar", 
+                   left_panel_ui('leftMain')),
       
       mainPanel(
         tabPanel("Urban Areas",
